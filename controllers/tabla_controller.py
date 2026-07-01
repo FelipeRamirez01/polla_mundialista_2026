b782c24a9662a3da6bd0b330c53fe2a8a7c37eb7
Felipe <f.ramirezb99@gmail.com>
2026-07-01 12:59:15 -0500

    update tabla de posiciones

--- controllers/tabla_controller.py
+++ controllers/tabla_controller.py
@@ -16,86 +16,123 @@ from models.partido_eliminacion import PartidoEliminacion
 @login_required
 def tabla_posiciones():
 
-    puntos_grupos = db.session.query(
-    Prediccion.usuario_id,
-    func.coalesce(
-        func.sum(Prediccion.puntos),
-        0
-    ).label('puntos_grupos')
-    ).group_by(
-        Prediccion.usuario_id
-    ).subquery()
+    # Puntos fase de grupos
+    puntos_grupos = (
+        db.session.query(
+            Prediccion.usuario_id,
+            func.coalesce(
+                func.sum(Prediccion.puntos),
+                0
+            ).label("puntos_grupos")
+        )
+        .group_by(Prediccion.usuario_id)
+        .subquery()
+    )
 
-    puntos_eliminacion = db.session.query(
+    # Puntos por resultados de eliminación
+    puntos_eliminacion = (
+        db.session.query(
             PartidoEliminacion.usuario_id,
-
-    (
-        func.coalesce(
-            func.sum(PartidoEliminacion.puntos),
-            0
+            func.coalesce(
+                func.sum(PartidoEliminacion.puntos),
+                0
+            ).label("puntos_eliminacion")
         )
-        +
-        func.coalesce(
-            func.sum(PartidoEliminacion.puntos_clasificacion),
-            0
+        .group_by(PartidoEliminacion.usuario_id)
+        .subquery()
+    )
+
+    # Puntos por clasificación
+    puntos_clasificacion = (
+        db.session.query(
+            PartidoEliminacion.usuario_id,
+            func.coalesce(
+                func.sum(PartidoEliminacion.puntos_clasificacion),
+                0
+            ).label("puntos_clasificacion")
         )
-    ).label("puntos_eliminacion")
-    ).group_by(
-        PartidoEliminacion.usuario_id
-    ).subquery()
+        .group_by(PartidoEliminacion.usuario_id)
+        .subquery()
+    )
 
-    tabla = db.session.query(
+    tabla = (
+        db.session.query(
 
-        Usuario.id,
+            Usuario.id,
+            Usuario.nombre,
 
-        Usuario.nombre,
-
-        func.coalesce(
-            puntos_grupos.c.puntos_grupos,
-            0
-        ).label('puntos_grupos'),
-
-        func.coalesce(
-            puntos_eliminacion.c.puntos_eliminacion,
-            0
-        ).label('puntos_eliminacion'),
-
-        (
             func.coalesce(
                 puntos_grupos.c.puntos_grupos,
                 0
-            )
-            +
-            func.coalesce(
-                puntos_eliminacion.c.puntos_eliminacion,
-                0
-            )
-        ).label('total_puntos')
+            ).label("puntos_grupos"),
 
-    ).outerjoin(
-        puntos_grupos,
-        Usuario.id == puntos_grupos.c.usuario_id
-    ).outerjoin(
-        puntos_eliminacion,
-        Usuario.id == puntos_eliminacion.c.usuario_id
-    ).filter(
-        Usuario.grupo_id == current_user.grupo_id
-    ).order_by(
-        (
-            func.coalesce(
-                puntos_grupos.c.puntos_grupos,
-                0
-            )
-            +
             func.coalesce(
                 puntos_eliminacion.c.puntos_eliminacion,
                 0
-            )
-        ).desc()
-    ).all()
+            ).label("puntos_eliminacion"),
+
+            func.coalesce(
+                puntos_clasificacion.c.puntos_clasificacion,
+                0
+            ).label("puntos_clasificacion"),
+
+            (
+                func.coalesce(
+                    puntos_grupos.c.puntos_grupos,
+                    0
+                )
+                +
+                func.coalesce(
+                    puntos_eliminacion.c.puntos_eliminacion,
+                    0
+                )
+                +
+                func.coalesce(
+                    puntos_clasificacion.c.puntos_clasificacion,
+                    0
+                )
+            ).label("total_puntos")
+
+        )
+        .outerjoin(
+            puntos_grupos,
+            Usuario.id == puntos_grupos.c.usuario_id
+        )
+        .outerjoin(
+            puntos_eliminacion,
+            Usuario.id == puntos_eliminacion.c.usuario_id
+        )
+        .outerjoin(
+            puntos_clasificacion,
+            Usuario.id == puntos_clasificacion.c.usuario_id
+        )
+        .filter(
+            Usuario.grupo_id == current_user.grupo_id
+        )
+        .order_by(
+            (
+                func.coalesce(
+                    puntos_grupos.c.puntos_grupos,
+                    0
+                )
+                +
+                func.coalesce(
+                    puntos_eliminacion.c.puntos_eliminacion,
+                    0
+                )
+                +
+                func.coalesce(
+                    puntos_clasificacion.c.puntos_clasificacion,
+                    0
+                )
+            ).desc(),
+            Usuario.nombre.asc()
+        )
+        .all()
+    )
 
     return render_template(
-        'tabla/tabla_posiciones.html',
+        "tabla/tabla_posiciones.html",
         tabla=tabla,
         grupo=current_user.grupo.nombre
     )
